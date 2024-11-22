9976030ab83fcfa81f801908bf42c1b238ae9d47
sHgNHwBO5c <112546648+sHgNHwBO5c@users.noreply.github.com>
2024-11-22 06:15:56 +0200

    添加了 用户认证 功能

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+console.log("===== Using ts-jest ======");
+
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	testPathIgnorePatterns: [
+		"<rootDir>/node_modules/",
+		"<rootDir>/e2e/",
+		"<rootDir>/docs/",
+		"<rootDir>/coverage/",
+		"<rootDir>/scripts/",
+		"<rootDir>/guides/",
+	],
+	coveragePathIgnorePatterns: ["<rootDir>/src/test/", "<rootDir>/e2e/"],
+	setupFilesAfterEnv: ["<rootDir>/src/test/jest.matchers.ts"],
+	collectCoverage: true,
+	collectCoverageFrom: ["./src/**"],
+	coverageThreshold: {
+		global: {
+			lines: 80,
+			functions: 80,
+			branches: 80,
+			statements: 80,
+		},
+	},
+};
